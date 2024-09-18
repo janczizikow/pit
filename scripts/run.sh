13b3a5b555875b3d93cacc1f577bf99ee8e7aa61
Jan Czizikow <jan.czizikow@gmail.com>
2024-09-18 23:19:38 +0200

    debug

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 set -e
 
 DB_DSN="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable"
+echo "before migrate command"
 migrate -path ./internal/database/migrations -database="$DB_DSN" up & PID=$!
 wait $PID
 
