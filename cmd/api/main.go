8e6f7c008479c2a75afc4e817575a3e986a361fd
Jan Czizikow <jan.czizikow@gmail.com>
2024-09-19 01:29:36 +0200

    more config for db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,17 +12,18 @@ import (
 
 func main() {
 	DB_HOST := os.Getenv("DB_HOST")
-	DB_PORT := 5432
+	DB_PORT := os.Getenv("DB_PORT")
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_NAME := os.Getenv("DB_NAME")
+	DB_SSL_MODE := os.Getenv("DB_SSL_MODE")
 	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	log.Info().Msg("starting server")
 	log.Info().Msg("connecting to postgres DB")
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_SSL_MODE)
 	db, err := database.Connect(dsn)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to connect to postgres DB")
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DB_DSN="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable"
+DB_DSN="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=$DB_SSLMODE"
 migrate -path ./internal/database/migrations -database="$DB_DSN" up & PID=$!
 wait $PID
 
