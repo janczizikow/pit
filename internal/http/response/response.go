e97a0effb589f4ef9a3e4e1eb29773c3bf15be38
Jan Czizikow <jan.czizikow@gmail.com>
2024-09-23 20:47:44 +0200

    fix: superfluous write header

--- internal/http/response/response.go
+++ internal/http/response/response.go
@@ -9,22 +9,20 @@ const contentType = "application/json; charset=utf-8"
 
 // WriteJSON sends a JSON response with status code.
 func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", contentType)
 	bytes, err := json.Marshal(data)
 	if err != nil {
-		writeErrorJSON(w, http.StatusInternalServerError, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		writeErrorJSON(w, err)
 		return
 	}
-
-	w.Header().Set("Content-Type", contentType)
 	w.WriteHeader(status)
 	_, err = w.Write(bytes)
 	if err != nil {
-		writeErrorJSON(w, http.StatusInternalServerError, err)
+		writeErrorJSON(w, err)
 	}
 }
 
-func writeErrorJSON(w http.ResponseWriter, status int, err error) {
-	w.Header().Set("Content-Type", contentType)
-	w.WriteHeader(status)
+func writeErrorJSON(w http.ResponseWriter, err error) {
 	w.Write([]byte(`{"error": "` + err.Error() + `"}`)) // nolint:golint,errcheck
 }
