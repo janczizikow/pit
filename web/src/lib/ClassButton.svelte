7de0735db1f7f77470288693be4a347160f919db
Jan Czizikow <jan.czizikow@gmail.com>
2024-09-27 12:39:38 +0200

    cooler class btns wip

--- web/src/lib/ClassButton.svelte
+++ web/src/lib/ClassButton.svelte
@@ -15,6 +15,9 @@
 		height: 64px;
 		width: 64px;
 		background-size: cover;
+		transition:
+			opacity 350ms,
+			filter 350ms;
 	}
 
 	@media only screen and (max-width: 800px) {
@@ -33,54 +36,67 @@
 		background-image: url('/classes/base/BARBARIAN.webp');
 	}
 	.barbarian-selected {
+		position: relative;
 		background-image: url('/classes/hover/BARBARIAN.webp');
+		filter: brightness(1.6);
 	}
 	.barbarian:hover {
 		background-image: url('/classes/hover/BARBARIAN.webp');
+		filter: brightness(1.6);
 	}
 	.druid {
 		background-image: url('/classes/base/DRUID.webp');
 	}
 	.druid-selected {
 		background-image: url('/classes/hover/DRUID.webp');
+		filter: brightness(1.6);
 	}
 	.druid:hover {
 		background-image: url('/classes/hover/DRUID.webp');
+		filter: brightness(1.6);
 	}
 	.necromancer {
 		background-image: url('/classes/base/NECROMANCER.webp');
 	}
 	.necromancer-selected {
 		background-image: url('/classes/hover/NECROMANCER.webp');
+		filter: brightness(1.6);
 	}
 	.necromancer:hover {
 		background-image: url('/classes/hover/NECROMANCER.webp');
+		filter: brightness(1.6);
 	}
 	.rogue {
 		background-image: url('/classes/base/ROGUE.webp');
 	}
 	.rogue-selected {
 		background-image: url('/classes/hover/ROGUE.webp');
+		filter: brightness(1.6);
 	}
 	.rogue:hover {
 		background-image: url('/classes/hover/ROGUE.webp');
+		filter: brightness(1.6);
 	}
 	.sorcerer {
 		background-image: url('/classes/base/SORCERER.webp');
 	}
 	.sorcerer-selected {
 		background-image: url('/classes/hover/SORCERER.webp');
+		filter: brightness(1.6);
 	}
 	.sorcerer:hover {
 		background-image: url('/classes/hover/SORCERER.webp');
+		filter: brightness(1.6);
 	}
 	.spiritborn {
 		background-image: url('/classes/base/SPIRITBORN.webp');
 	}
 	.spiritborn-selected {
 		background-image: url('/classes/hover/SPIRITBORN.webp');
+		filter: brightness(1.6);
 	}
 	.spiritborn:hover {
 		background-image: url('/classes/hover/SPIRITBORN.webp');
+		filter: brightness(1.6);
 	}
 </style>
